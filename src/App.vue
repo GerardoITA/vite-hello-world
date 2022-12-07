402b4959fe974662909ee8dcccd053cb44ebaa69
GerardoITA <gerardo.giusti@tiscali.it>
2022-12-07 16:42:17 +0100

    finito

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import AppTitle from './components/AppTitle.vue'
 </script>
 
 <template>
@@ -8,12 +9,13 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      
+      <AppTitle msg="La mia prima app con Vite"/>
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+
   </main>
 </template>
 
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+    msg: {
+        type: String,
+        required: true
+    }
+})
+</script>
+<template>
+<h1 class="h1">{{ msg }}</h1>
+</template>
+
+<style scoped>
+    .h1 {color: red }
+</style>
